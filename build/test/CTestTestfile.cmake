a5dd76824da6bdbb7e414aa5e0b1d69f4582df5d
Robin Truong <robintrg007@gmail.com>
2020-05-15 17:38:39 -0700

    Current Build folder

--- build/test/CTestTestfile.cmake
+++ build/test/CTestTestfile.cmake
@@ -4,5 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(BitStreamTests "C:/Users/robin/Google Drive/SFU/Computing Science/Huffman/build/test/BitStreamTests.exe")
-set_tests_properties(BitStreamTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/robin/Google Drive/SFU/Computing Science/Huffman/test/CMakeLists.txt;16;add_test;C:/Users/robin/Google Drive/SFU/Computing Science/Huffman/test/CMakeLists.txt;0;")
+add_test(HuffmanTest "C:/Users/robin/Google Drive/SFU/Computing Science/Huffman/build/test/HuffmanTest.exe")
+set_tests_properties(HuffmanTest PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/robin/Google Drive/SFU/Computing Science/Huffman/test/CMakeLists.txt;16;add_test;C:/Users/robin/Google Drive/SFU/Computing Science/Huffman/test/CMakeLists.txt;0;")
