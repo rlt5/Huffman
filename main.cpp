8ed3619149422ec86446d457840424e26f77f796
Robin Truong <robintrg007@gmail.com>
2020-06-08 16:58:51 -0700

    initial commit for main function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
-// #include "BitStream.cpp"
+#include "BitStream.hpp"
 #include <gtest/gtest.h>
 #include "util.hpp"
-using namespace std;
 
-int main(){
-    cout << add(2,2) << endl;
+int main(int argc, char* argv[]){
+    
+    // Check valid number of arguments
+    if ( argc != 4 ){
+        std::cout << "Incorrect input format! (Ex: ./huffman -c source.txt destination.huffman) " << std::endl;
+        return 0;
+    }
+    
+    // Check valid mode input
+    int mode = -1;
+    if ( argv[1] == "-c" ) mode == 0;
+    else if ( argv[1] == "-d" ) mode == 1;
+    else {
+        std:: cout << "The mode input is incorrect. Please enter either -c for compression or -d for decompression" << std::endl;
+        return 0;
+    }
+
+    // Load File
+    BitStream bitStream(mode);
+    bitStream.loadFile(argv[2]);
+    
+
+
     return 0;
 }
